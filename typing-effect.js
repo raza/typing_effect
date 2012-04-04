5405ecbc5b92a16936a7a2cbbb20255a7af87900
Raza Ali <tauraz@gmail.com>
2012-04-05 00:27:12 +0400

    initial commit for the typing_effect feature

---
+++ typing-effect.js
@@ -0,0 +1,36 @@
+typing_effect = function(string, dom_id, do_erase){
+
+  var string_length = string.length;
+  var index = 0;
+  var setInterval_id = 0;
+
+  type_string = function(){
+
+    setInterval_id = setInterval(function(){
+      val = string[index];
+      if(index >= string_length){
+        clearInterval(setInterval_id);
+        index = $(dom_id).html().length - 1;
+        if(do_erase == true)
+          setTimeout('erase_string()', 500);
+          return true;
+      }
+      $('#typing_list').append(val);
+      index += 1;
+    }, 50);
+  }
+
+  erase_string = function(){
+    setInterval_id = setInterval(function(){
+      val = $(dom_id).html().substring(0, index);
+      $(dom_id).html(val);
+      if(index <= 0){
+        clearInterval(setInterval_id);
+        return true;
+      }
+      index -= 1;
+    }, 50);
+  }
+
+  type_string();
+}
