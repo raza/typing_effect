4e9c6cbd67ee10a410d94560b71bc339780e611c
Raza Ali <tauraz@gmail.com>
2012-04-05 09:56:11 +0400

    rotate strings functionality added with a array of strings and do_erase functionality removed

--- typing-effect.js
+++ typing-effect.js
@@ -1,34 +1,44 @@
-typing_effect = function(string, dom_id, do_erase){
+typing_effect = function(strings_array, dom_id){
 
+  var strings_index = 0;
+  var strings_length = strings_array.length;
+  var string = strings_array[strings_index];
   var string_length = string.length;
-  var index = 0;
+  var string_index = 0;
   var setInterval_id = 0;
 
   type_string = function(){
 
     setInterval_id = setInterval(function(){
-      val = string[index];
-      if(index >= string_length){
+      val = string[string_index];
+      if(string_index >= string_length){
         clearInterval(setInterval_id);
-        index = $(dom_id).html().length - 1;
-        if(do_erase == true)
+        string_index = $(dom_id).html().length - 1;
+        if(strings_index < (strings_length - 1)){
           setTimeout('erase_string()', 500);
           return true;
+        }
       }
       $(dom_id).append(val);
-      index += 1;
+      string_index += 1;
     }, 50);
   }
 
   erase_string = function(){
     setInterval_id = setInterval(function(){
-      val = $(dom_id).html().substring(0, index);
+      val = $(dom_id).html().substring(0, string_index);
       $(dom_id).html(val);
-      if(index <= 0){
+      if(string_index <= 0){
+        strings_index += 1;
         clearInterval(setInterval_id);
+        if(strings_index < strings_length){
+          string_index = 0;
+          string = strings_array[strings_index];
+          setTimeout("type_string()", 500);
+        }
         return true;
       }
-      index -= 1;
+      string_index -= 1;
     }, 50);
   }
 
